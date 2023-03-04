aef657c09f94485be3979bacd9c1f1ea6fe4fd46
bwgjoseph <josephgan@live.com.sg>
2023-03-04 23:53:09 +0800

    remove @WritingConverter

--- src/main/java/com/bwgjoseph/sdmdocrefbug/ReferenceAbleConverter.java
+++ src/main/java/com/bwgjoseph/sdmdocrefbug/ReferenceAbleConverter.java
@@ -2,10 +2,9 @@ package com.bwgjoseph.sdmdocrefbug;
 
 import org.bson.Document;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.mongodb.core.mapping.DocumentPointer;
 
-@WritingConverter
+// @WritingConverter
 public class ReferenceAbleConverter implements Converter<ReferenceAble, DocumentPointer<Document>> {
 
     @Override
